f9f8e43a7995feaf4ce3dbaeae7064e8b603f84e
Devashish Kumar <85827919+deva8445@users.noreply.github.com>
2022-03-09 11:15:28 +0530

    Assignment 1

---
+++ Calculator/main.js
@@ -0,0 +1,9 @@
+// console.log("Hello World");
+const add=require(`./add`);
+const divide = require("./divide");
+const multiply = require("./multiply");
+const sub = require("./subtract");
+console.log(add());
+console.log(sub(10,20));
+console.log(multiply(20,2));
+console.log(divide(2000,5));
